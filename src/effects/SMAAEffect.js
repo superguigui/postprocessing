50d7df3def9bd20d85010a7925759bd40f124b0b
Guillaume Gouessan <guillaume.gouessan@gmail.com>
2022-03-22 11:05:39 +0100

    fix: event object

--- src/effects/SMAAEffect.js
+++ src/effects/SMAAEffect.js
@@ -144,7 +144,7 @@ export class SMAAEffect extends Effect {
 			const onSMAAImageLoaded = () => {
 
 				nbImagesLoaded++;
-				if (nbImagesLoaded === 2) this.dispatchEvent("load");
+				if (nbImagesLoaded === 2) this.dispatchEvent({ type: "load" });
 
 			}
 
